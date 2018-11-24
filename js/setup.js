153516967f91f5a2c3a6796a34a224d4b7a32cd0
Andrei <l2.ls@yandex.ru>
2018-11-24 12:47:29 +0300

    Добавлена отрисовка элементов на страницу

--- js/setup.js
+++ js/setup.js
@@ -1,19 +1,12 @@
 'use strict';
 
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
-document.querySelector('.setup-similar').classList.remove('hidden');
-
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-  .content
-  .querySelector('.setup-similar-item');
-
 var WIZARD_FIRSTNAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_LASTNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
 
 var nameRandomize = function (firstNames, lastNames) {
   var i = Math.floor(Math.random() * firstNames.length);
@@ -35,24 +28,41 @@ var colorRandomize = function (colors) {
   return randomColor;
 };
 
-var randomWizards = function (count) {
-  var wizards = [];
+var wizardsRandomize = function (count) {
+  var wizardsList = [];
   for (var i = 0; i < count; i++) {
-    wizards.push(Object());
-    wizards[i].name = nameRandomize(WIZARD_FIRSTNAMES, WIZARD_LASTNAMES);
-    wizards[i].coatColor = colorRandomize(WIZARD_COAT_COLORS);
-    wizards[i].eyesColor = colorRandomize(WIZARD_EYES_COLORS);
+    wizardsList.push(Object());
+    wizardsList[i].name = nameRandomize(WIZARD_FIRSTNAMES, WIZARD_LASTNAMES);
+    wizardsList[i].coatColor = colorRandomize(WIZARD_COAT_COLORS);
+    wizardsList[i].eyesColor = colorRandomize(WIZARD_EYES_COLORS);
   }
-  return wizards;
+  return wizardsList;
 };
 
-var wizards = randomWizards(4);
-
-for (var i = 0; i < wizards.length; i++) {
+var renderWizard = function (wizard) {
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+    .content
+    .querySelector('.setup-similar-item');
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label').textContent = wizards[i].name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-  similarListElement.appendChild(wizardElement);
-}
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var drawWizards = function () {
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var fragment = document.createDocumentFragment();
+  var wizards = wizardsRandomize(4);
+
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+};
+
+drawWizards();
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
